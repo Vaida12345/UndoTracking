6ca21254a433cc82f5fbb9303a570685a35af613
Vaida <developer@vaidaf.net>
2025-08-22 21:29:11 +1000

    fixed build on both swift 6.2 and 6.1

--- Sources/withUndoTracking.swift
+++ Sources/withUndoTracking.swift
@@ -23,6 +23,7 @@ import SwiftUI
 /// - Parameters:
 ///   - undoManager: Pass the `UndoManager` from the environment.
 ///   - builder: The closure consists of the undoable actions.
+#if swift(>=6.2)
 @MainActor
 public func withUndoTracking<T>(
     _ undoManager: UndoManager?,
@@ -54,3 +55,35 @@ public func withUndoTracking<T>(
     
     component.action(component.target, _withAnimation, _registerUndo)
 }
+#else
+public func withUndoTracking<T>(
+    _ undoManager: UndoManager?,
+    builder: () -> UndoComponent<T>
+) {
+    nonisolated(unsafe) let undoManager = undoManager
+    nonisolated(unsafe) let component = builder()
+    
+    if let title = component.actionName {
+        undoManager?.setActionName(title.localized())
+    }
+    let _withAnimation: (() -> Void) -> Void
+    switch component.animate {
+    case true:
+        _withAnimation = { block in withAnimation(.default, { block() }) }
+    case false:
+        _withAnimation = { $0() }
+    }
+    
+    let _registerUndo: (@escaping () -> UndoComponent<T>) -> Void = { builder in
+        nonisolated(unsafe) let builder = builder
+        
+        undoManager?.registerUndo(withTarget: component.target) { target in
+            withUndoTracking(undoManager) {
+                component.replacingAction(with: builder().action)
+            }
+        }
+    }
+    
+    component.action(component.target, _withAnimation, _registerUndo)
+}
+#endif
